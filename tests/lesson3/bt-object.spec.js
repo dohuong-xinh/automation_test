37e706f9d5fe45040c10b67c73f3f2c0c78af85d
Do Huong <dohuong@Mac-mini-cua-Do.fpt>
2026-04-07 22:16:10 +0700

    lam bai tap object

---
+++ tests/lesson3/bt-object.spec.js
@@ -0,0 +1,49 @@
+//bai 1
+const car = {"make": "Toyota", "model": "Corolla", "year" : "2021"};
+console.log(car.year)
+
+//bai 2
+const person = {"name": "huong",
+    "address":
+    {
+        "street": "Phu Nghia",
+        "city": "Ha Noi",
+        "country": "Viet Nam"
+    }
+};
+console.log(person.name + " " + person.address.street)
+
+//bai 3
+const student = {"name": "Huongxinh", 
+    "grades":
+    {
+        "math": 1,
+        "english": 2
+    }
+};
+console.log("Diem toan la " + student["grades"]["math"])
+
+//bai 4
+const settings = {"volumn": 12, "brightness": 100
+}
+settings.volumn = 50;
+console.log(settings.volumn)
+
+//bai 5
+const bike = {"make": "Viet Nhat", "model": "2026"}
+bike.color = "Pink"
+console.log(bike)
+
+//bai 6
+const employee = {"name": "Huongxinh", "age": "20"}
+delete employee.age
+console.log(employee)
+
+//bai 7
+const school = {
+    classA: ["An", "Binh", "Chau"],
+    classB: ["Dao", "Huong", "Giang"]
+}
+console.log(school.classA + "," + school.classB)
+
+
